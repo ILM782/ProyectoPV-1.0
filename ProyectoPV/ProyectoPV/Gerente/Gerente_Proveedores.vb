74bee16e25916358c1101e5d256393782ee50abc
fedestorti <fede_storti31@hotmail.com>
2018-10-05 00:42:06 -0300

    hola

--- ProyectoPV/ProyectoPV/Gerente/Gerente_Proveedores.vb
+++ ProyectoPV/ProyectoPV/Gerente/Gerente_Proveedores.vb
@@ -90,7 +90,56 @@
     End Sub
 
     Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
-        Me.ProveedorTableAdapter.FillByProveedor(Me.MayoristaBaseDeDatosDataSet.Proveedor, TextBox1.Text)
+        'arranco con una grilla sin enlazar, traigo las dos grilla enlazada, edito la grilla con las columnas
+        'case para elejir atributo con un combobox
+        Dim fila, contador, Total, filaConsulta, art As Integer
+        Dim Cadena As String
+        fila = 0 'ir bajando lineas en el datagrill
+        Total = 0 'sumo los subtotales
+        filaConsulta = 0 'para hacer la consulta de la otra tabla
+        DataGridView1.Rows.Clear() 'limpio la grilla cada ves que ingreso un dato 
+        ProveedorBindingSource.MoveFirst() 'muevo al primer registro de la tabla
+
+        Do
+            Try
+                Cadena = ProveedorBindingSource.Current("Id_articulo").ToString.Substring(0, Len(TextBox1.Text))
+                'ejemplo de error descripcion valor "maria"    caja de texto "mariano" len da 7 y maria tiene solo 5 tira error
+            Catch ex As Exception 'ante cualquier error
+                Cadena = ""
+
+            End Try
+            'ucase para pasarlo a mayuscula
+
+            If UCase(Cadena) = UCase(TextBox1.Text) Then
+                DataGridView1.Rows.Add() 'columna
+                DataGridView1.Item(0, fila).Value = ProveedorBindingSource.Current("ID_Proveedor")
+                DataGridView1.Item(1, fila).Value = ProveedorBindingSource.Current("Nombre_Empresa")
+                art = ProveedorBindingSource.Current("ID_Proveedor")
+
+                'busco la cantidad apartir del id en la tabla blister
+                filaConsulta = Me.LocalidadBindingSource.Find("ID_Localidad", ProveedorBindingSource.Current("Localidad")) 'busco relacion id-descripcion blister
+                Me.LocalidadBindingSource.Position = filaConsulta
+                DataGridView1.Item(2, fila).Value = Me.LocalidadBindingSource.Current("Localidad_Localidad") 'pego la descripcion no el id
+
+                'continuo pegando datos de la tabla stock
+                DataGridView1.Item(3, fila).Value = ProveedorBindingSource.Current("Provincia_Localidad")
+                DataGridView1.Item(4, fila).Value = ProveedorBindingSource.Current("CP_Localidad")
+                'pego en la grilla la multiplicacion de cantidad por precio
+                DataGridView1.Item(5, fila).Value = ProveedorBindingSource.Current("Domicilio_Proveedor")
+                DataGridView1.Item(6, fila).Value = ProveedorBindingSource.Current("Telefono_Proveedor")
+                DataGridView1.Item(6, fila).Value = ProveedorBindingSource.Current("Correo_Proveedor")
+
+            End If
+            contador = contador + 1
+            ProveedorBindingSource.MoveNext()
+        Loop Until contador = ProveedorBindingSource.Count
+
+
+        'creo una fila final con el total a facturar
+        DataGridView1.Item(4, fila).Value = "Total a facturar"
+        DataGridView1.Item(5, fila).Value = Total
+
+        TextBox1.Focus()
     End Sub
 
     Private Sub Btn_Eliminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btn_Eliminar.Click
@@ -127,4 +176,6 @@
             e.Handled = True
         End If
     End Sub
+
+    
 End Class
